a328ac63dac74b5dfef3ebea22f82708242ec606
Battugs-01 <tugsking2002@gmail.com>
2024-08-02 17:13:12 +0800

    [product page 50%]

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import CellAction from "./cell-action";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+  price: string;
+  category: string;
+  color: string;
+  size: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "color",
+    header: "color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        ></div>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "size",
+    header: "size",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
